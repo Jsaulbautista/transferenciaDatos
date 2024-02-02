4751f3dbe8c6ef93883199f47a5ce3c1f66c3ffe
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2024-02-02 00:05:11 -0500

    se añadió el comboBox para que funcione

--- src/combox.java
+++ src/combox.java
@@ -1,17 +1,22 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class combox {
     private JComboBox combox;
     private JLabel titulo;
     public JPanel comboBox;
+    private JLabel resultado;
 
 
     public combox() {
         combox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String color = "color";
+                color = (combox.getSelectedItem()).toString();
+                resultado.setText(color);
 
             }
         });
